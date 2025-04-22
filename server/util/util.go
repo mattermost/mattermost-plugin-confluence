ea9ef869e7494adef58579be09e54c14d2cef46c
kshitij katiyar <kshitij.katiyar@brightscout.com>
2025-04-22 15:28:36 +0530

    Replacing html dependency

--- server/util/util.go
+++ server/util/util.go
@@ -9,7 +9,7 @@ import (
 	"regexp"
 	"strings"
 
-	html "github.com/levigross/exp-html"
+	html "golang.org/x/net/html"
 	"github.com/mattermost/mattermost/server/public/model"
 
 	"github.com/mattermost/mattermost-plugin-confluence/server/config"
